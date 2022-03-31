3a6580abea429ffed9c884df17c87af8ded23c2b
makometr <makometr@yandex.ru>
2022-03-31 19:32:49 +0300

    tests for parser added:

--- services/calc-worker/parser/parser.go
+++ services/calc-worker/parser/parser.go
@@ -8,16 +8,26 @@ import (
 )
 
 var (
+	// ErrParseExpression occurres when expression is not like "NumberOperationNumber"
 	ErrParseExpression = errors.New("error in parsing expression")
+
+	// ErrCalculationExpression occurres with incorrect arithmetic operations, divide by zero for example
+	ErrCalculationExpression = errors.New("error in calculations")
 )
 
+// CalculateSimpleExpression returns result of arithmetic expression
 func CalculateSimpleExpression(expression string) (string, error) {
 	parsed, err := parseBinaryExpression(expression)
 	if err != nil {
 		return "", fmt.Errorf("%w: %v", ErrParseExpression, err)
 	}
 
-	return parsed.calculate(), nil
+	result, err := parsed.calculate()
+	if err != nil {
+		return "", fmt.Errorf("%w: %v", ErrCalculationExpression, err)
+	}
+
+	return result, nil
 }
 
 type operation int
@@ -45,7 +55,7 @@ type parseResult struct {
 
 func parseBinaryExpression(expr string) (parseResult, error) {
 	operIndex := strings.IndexFunc(expr, func(r rune) bool {
-		for sign, _ := range operations {
+		for sign := range operations {
 			if r == sign {
 				return true
 			}
@@ -68,7 +78,7 @@ func parseBinaryExpression(expr string) (parseResult, error) {
 	return parseResult{lhs: operandType(lhsNum), rhs: operandType(rhsNum), operation: operations[rune(expr[operIndex])]}, nil
 }
 
-func (pr parseResult) calculate() string {
+func (pr parseResult) calculate() (string, error) {
 	var result operandType
 
 	switch pr.operation {
@@ -79,8 +89,11 @@ func (pr parseResult) calculate() string {
 	case multi:
 		result = pr.lhs * pr.rhs
 	case divide:
+		if pr.rhs == 0 {
+			return "", fmt.Errorf("dividing by zero")
+		}
 		result = pr.lhs / pr.rhs
 	}
 
-	return strconv.FormatInt(int64(result), 10)
+	return strconv.FormatInt(int64(result), 10), nil
 }
